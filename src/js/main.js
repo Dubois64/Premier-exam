49ed3e04f8492722c1d912ce40b24feb793b718c
dubois64 <constance.dubois@studentfr.ch>
2025-09-16 11:54:19 +0200

    switch light dark mode début

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,14 @@ function toggleMenu() {
   menu.classList.toggle("is-active");
 }
 burger.addEventListener("click", toggleMenu);
+
+const toggleDarkModeBtn = document.querySelector("#dark-mode-btn");
+const toggleLightModeBtn = document.querySelector("#light-mode-btn");
+
+toggleDarkModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+});
+
+toggleLightModeBtn.addEventListener("click", function () {
+  document.querySelector("html").setAttribute("data-theme", "light");
+});
